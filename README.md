6078587c609feb5c4c2c04f7811b09d9e6c243b4
soobrosa <soobrosa@gmail.com>
2015-10-02 14:47:50 +0200

    Short description.

--- README.md
+++ README.md
@@ -1,9 +1,39 @@
 
 
-# A framework for nightly data processing using GNU Make
+# A framework for nightly batch data processing using GNU Make
+
+History
+=
+
+In 2013 [@torsten](https://github.com/torsten) wanted to set up a batch job manager for the Wunderlist data pipeline. Evaluating Amazon Data Flow, Oozie and Luigi all seemed to be overkill. Inspired by [Mike Bostock](http://bost.ocks.org/mike/make/) he started out rolling one `Makefile` started by a `cronjob`. In two years this tool has been grown to accomodate the needs and in 2015 we open sourced the skeleton of the add-ons that make your life easier going old skool and seventies or KISS, you may say.
+
+This repo is currently in production reaching out to dozens of sources and running hundreds of SQLs. If you are interested in our setup, check out [this presentation](http://www.slideshare.net/soobrosa/6w-bp-datashow).
+
+Features
+=
+- has a Makefile wrapper that gets triggered by cron, tries to run it at most MAX_ATTEMPTS times and logs to output so cron can send an email,
+- runs all `make` targets in a tracking shell, so timing information, output and errors could be logged,
+- produces log files that can be synchronized to a file store (we use S3),
+- has a timer script `run_at` for `cron` like target timing,
+- has a script to inject conditionals, variableas and Ruby logic into SQL.
+
+What's in `lib`
+=
+You should run `run_workflow.sh` that will wrap `Makefile`.
+
+What's in `tests`
+=
+Running `./tests/run.sh` will
+
+- test for makefile target for production,
+- collect unused or wrong makefile targets,
+- check unused files in the script folder,
+- check unused template variables.
+
+There's also a test for checking whether log size is below or above threshold.
 
 ## License
 
-Copyright © 2013-2015 6 Wunderkinder GmbH.
+Copyright © 2013-2015 6Wunderkinder GmbH.
 
 Distributed under the MIT License.
--- lib/boilerplate.mk
+++ lib/boilerplate.mk
@@ -4,7 +4,7 @@ endif
 
 TMP_TOKEN:=$(shell date +%H%M%S)
 
-# `date` behaves different on Mac and Linux, so we need to check the platform here:
+# `date` behaves different on Mac and Linux, so we need to check the platform here.
 ifeq ($(shell uname -s),Darwin)
   YESTERDAY:=$(shell date -v-1d +%Y-%m-%d)
   YESTERDAY_SLASHED:=$(shell date -v-1d +%Y/%m/%d)
@@ -17,26 +17,26 @@ endif
 # make processes do not overwrite other temp files.
 TMP_OUT=$@.$(TMP_TOKEN).tmp
 
-# Define the tracking shell (you can redefined in your Makefile)
+# Define the tracking shell (you can redefine it in your Makefile).
 SHELL=./night-shift/lib/tracking_shell.py --target $@ --date $(TODAY)
 
 .PHONY: all nuke scaffold backup clean-test cleanup
 
-# All target is disabled
+# All target is disabled.
 all:
 	@echo Specify target
 
-# Scaffolding directory structure (please redefine)
+# Scaffolding directory structure (please redefine).
 scaffold:: logs/$(TODAY)/
 
-# Folder for logs
+# Folder for logs.
 logs/$(TODAY)/:
 	mkdir -p $@
 
 # Clean-up targets. WARNING: Use nuke only during development!
 nuke::
 
-# Cleanup terminates any pending resources. (please redefine)
+# Cleanup terminates any pending resources (please redefine).
 cleanup::
 
 # Clean-up test folders.
@@ -44,11 +44,11 @@ clean-test::
 	@rm -rf logs/_test_
 	@mkdir -p logs/_test_
 
-# Lists all available targets from the makefile
+# Lists all available targets from the makefile.
 list-all-targets:
 	@egrep -h '^[a-zA-Z][^# :=]+:([^=]|$$)' Makefile *.mk | cut -d: -f1 | sed -e 's/$$(TODAY)/'$(TODAY)'/g'
 
 # Backup after the target execution is finished (please redefine)
-# WARNING: Order is important. has to be the last target in this dependency list!
-# NOTE: it should be the same ones as you can find in the `config/night_shift.sh`.
+# WARNING: Order is important. This has to be the last target in this dependency list!
+# It should be the same ones as you can find in the `config/night_shift.sh`.
 backup::
